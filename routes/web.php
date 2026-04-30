9c27733772971ca8f8661336376a2e253c6632ca
marwaaulialukman956@gmail.com <111261372+mrwaaulia@users.noreply.github.com>
2026-04-30 21:34:34 +0700

    feat: add role selection page

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 
-// buatkan semua route untuk redirect ke masing masing halaman
+/*
+|--------------------------------------------------------------------------
+| LANDING
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.landing.role-select');
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| AUTH
+|--------------------------------------------------------------------------
+*/
+
+// Pengelola
+Route::get('/pengelola/login', function () {
+    return view('pages.auth.pengelola.login-pengelola');
+})->name('login.pengelola');
+
+//Penghuni
+Route::get('/penghuni/login', function () {
+    return view('pages.auth.penghuni.login-penghuni');
+})->name('login.penghuni');
